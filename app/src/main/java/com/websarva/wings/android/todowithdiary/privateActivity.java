c68e04af3dd6af1b0506121a5c64bb15c8997258
HanadaRiko <57882283+HanadaRiko@users.noreply.github.com>
2019-12-10 16:48:24 +0900

    全削除機能の実装

--- app/src/main/java/com/websarva/wings/android/todowithdiary/privateActivity.java
+++ app/src/main/java/com/websarva/wings/android/todowithdiary/privateActivity.java
@@ -1,24 +1,31 @@
 package com.websarva.wings.android.todowithdiary;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
-import android.support.v4.view.MenuItemCompat;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.websarva.wings.android.todowithdiary.DatabaseHelper.TABLE_NAME;
 
 public class privateActivity extends AppCompatActivity {
 
@@ -26,6 +33,8 @@ public class privateActivity extends AppCompatActivity {
     private TodoAdapter todo;
     private List<Model> list = new ArrayList<Model>();
     private ListView lv_private;
+
+    TodoListAdapter rowAdapater;
 
     private ArrayAdapter<String> adapter;
 
@@ -64,73 +73,33 @@ public class privateActivity extends AppCompatActivity {
         //ListView取得
         lv_private = findViewById(R.id.lv_private);
 
-        TodoListAdapter rowAdapater = new TodoListAdapter(this, 0, list);
+        rowAdapater = new TodoListAdapter(this, 0, list);
+
 
         lv_private.setAdapter(rowAdapater);
 
+
+        //全削除ボタン
+        Button all_delete_btn = findViewById(R.id.all_delete_btn);
+        all_delete_btn.setOnClickListener(new ListItemClickListener());
     }
 
+    private class ListItemClickListener implements View.OnClickListener {
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//
-//        // メニューボタンを追加
-//        MenuItem item = menu.add("NEW");
-//        item.setIcon(android.R.drawable.ic_input_add);
-//        // メニューのイベント定義
-//        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-//            @Override
-//            public boolean onMenuItemClick(MenuItem item) {
-//
-//                // インテントのインスタンス生成
-//                Intent intent = new Intent(privateActivity.this, registerPrivateActivity.class);
-//                // 次画面のアクティビティ起動
-//                startActivity(intent);
-//
-//                return false;
-//            }
-//        });
-//        // 画面に表示する処理
-//        MenuItemCompat.setShowAsAction(item,
-//                MenuItemCompat.SHOW_AS_ACTION_IF_ROOM);
-//
-//        return true;
-//    }
+        @Override
+        public void onClick(View view) {
+            DeleteDialogFragment dialogFragment = new DeleteDialogFragment(privateActivity.this);
+            dialogFragment.show(getSupportFragmentManager(), "DeleteDialogFragment");
 
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.bt_registerPrivate) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-
+        }
+    }
 
     @Override
     protected void onRestart() {
 
         Log.d("activity", "onRestart");
 
-        list.clear();
-        Cursor c = todo.getAllList();
-        if (c.moveToFirst()) {
-            do {
-                Model item = new Model();
-                item.set_id(c.getInt(c.getColumnIndex("_id")));
-                item.setTodo(c.getString(c.getColumnIndex("todo")));
-                list.add(item);
-            } while (c.moveToNext());
-        }
+        loadPrivateListView();
 
         // 紐付け
         lv_private = (ListView) findViewById(R.id.lv_private);
@@ -142,6 +111,19 @@ public class privateActivity extends AppCompatActivity {
         lv_private.setAdapter(rowAdapater);
 
         super.onRestart();
+    }
+
+    private void loadPrivateListView() {
+        list.clear();
+        Cursor c = todo.getAllList();
+        if (c.moveToFirst()) {
+            do {
+                Model item = new Model();
+                item.set_id(c.getInt(c.getColumnIndex("_id")));
+                item.setTodo(c.getString(c.getColumnIndex("todo")));
+                list.add(item);
+            } while (c.moveToNext());
+        }
     }
 
 
@@ -157,6 +139,66 @@ public class privateActivity extends AppCompatActivity {
     //戻るボタン処理
     public void onBackButtonClick(View view) {
         finish();
+    }
+
+    @SuppressLint("ValidFragment")
+    public static class DeleteDialogFragment extends DialogFragment {
+        privateActivity privateActivity;
+
+        public DeleteDialogFragment(privateActivity privateActivity) {
+            this.privateActivity = privateActivity;
+        }
+
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+            builder.setTitle(R.string.dialog_title);
+
+            builder.setMessage(R.string.dialog_message);
+
+            builder.setPositiveButton(R.string.dialog_btn_ok, new DeleteDialogFragment.DialogButtonClickListener());
+
+            builder.setNegativeButton(R.string.dialog_btn_ng, new DeleteDialogFragment.DialogButtonClickListener());
+
+            AlertDialog dialog = builder.create();
+            return dialog;
+        }
+
+        private class DialogButtonClickListener implements DialogInterface.OnClickListener {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                String msg = "";
+                
+                //全削除dialog分岐
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        msg = getString(R.string.dialog_ok_toast);
+                        privateActivity.list.clear();
+
+                        DatabaseHelper helper = new DatabaseHelper(privateActivity);
+                        SQLiteDatabase db = helper.getWritableDatabase();
+                        try {
+                            String sqlDelete = "DELETE FROM " + TABLE_NAME + " ;";
+                            SQLiteStatement stmt = db.compileStatement(sqlDelete);
+                            stmt.executeUpdateDelete();
+                        } finally {
+                            db.close();
+                        }
+                        privateActivity.loadPrivateListView();
+                        privateActivity.rowAdapater.notifyDataSetChanged();
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        msg = getString(R.string.dialog_ng_toast);
+                }
+                Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
+
+
+            }
+        }
+
     }
 
 }
